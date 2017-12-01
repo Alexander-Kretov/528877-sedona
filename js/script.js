d3d3b52adf62272d35676a8712d9556765976b2f
AxElander <skatermylo@mail.ru>
2017-12-01 07:29:10 +0300

    Оживление и минификация

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 var formpopup = document.querySelector(".form-search");
 var link = document.querySelector(".hotel-search-btn");
+var formjs = document.querySelector(".form-onjs");
 var chekin = formpopup.querySelector("[name=check-in]");
 
-link.addEventListener("click", function(event) {
-    event.preventDefault();
-    formpopup.classList.toggle("form-block");
-    chekin.focus();
+formpopup.classList.add("form-onjs");
+link.addEventListener("click", function(evt) {
+  formpopup.classList.toggle("form-onjs");
+  evt.preventDefault();
+  chekin.focus();
 });
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (formpopup.classList.contains("form-search")) {
+      formpopup.classList.add("form-onjs");
+    }
+  }
+})
